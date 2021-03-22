545f7071676d4649e744b6033be5ba6b7badaa57
Charrier Clement <clement.charrier@etu.univ-orleans.fr>
2021-03-22 03:43:32 +0100

    Creation des pages

---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/Pages/HomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PizzaIllico.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            BindingContext = new HomeViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/ViewModels/HomeViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PizzaIllico.Mobile.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public HomeViewModel()
+        {
+
+        }
+        async void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+            Button pressed = sender as Button;
+            switch (pressed.Tag)
+            {
+                case "BtnConnexion":
+                    await Navigation.PushAsync(new ConnexionPage());
+                    break;
+                case "BtnEnregistrement":
+                    await Navigation.PushAsync(new UserPage(true));
+                    break;
+                case "BtnCommande":
+                    await Navigation.PushAsync(new ShopListPage());
+                    break;
+                case "BtnPanier":
+                    await Navigation.PushAsync(new OrderPage(true));
+                    break;
+                case "BtnAnciennesCommandes":
+                    await Navigation.PushAsync(new OrderListPage());
+                    break;
+                case "BtnCompte":
+                    await Navigation.PushAsync(new UserPage(false));
+                    break;
+            }
+            
+        }
+    }
+}
