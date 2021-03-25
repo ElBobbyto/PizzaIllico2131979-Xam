efe772a5b961bfa55d959544f076b40fcc4ccc3a
Charrier Clement <clement.charrier@etu.univ-orleans.fr>
2021-03-25 22:03:07 +0100

    Update ApiService.cs

--- PizzaIllico.Mobile/PizzaIllico.Mobile/Services/ApiService.cs
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/Services/ApiService.cs
@@ -26,5 +26,26 @@ namespace PizzaIllico.Mobile.Services
 
 	        return JsonConvert.DeserializeObject<TResponse>(content);
         }
+        public async Task<TResponse> Post<TResponse>(string url, string postcontent)
+        {
+	        //requete post
+	        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, HOST + url);
+	        request.Headers.Add("Accept", "application/json");
+	        request.Content = new StringContent(postcontent, System.Text.Encoding.UTF8, "application/json");
+	        //réponse
+	        HttpResponseMessage response = await _client.SendAsync(request);
+	        string content = await response.Content.ReadAsStringAsync();
+	        return JsonConvert.DeserializeObject<TResponse>(content);
+        }
+        public async Task<TResponse> Put<TResponse>(string url, string putcontent)
+        {
+	        //requete put
+	        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, HOST + url);
+	        request.Content = new StringContent(putcontent, System.Text.Encoding.UTF8, "application/json");
+	        //réponse
+	        HttpResponseMessage response = await _client.SendAsync(request);
+	        string content = await response.Content.ReadAsStringAsync();
+	        return JsonConvert.DeserializeObject<TResponse>(content);
+        }
     }
 }
