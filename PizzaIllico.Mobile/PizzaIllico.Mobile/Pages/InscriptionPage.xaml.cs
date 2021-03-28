d90253cf64dd5ea33d7d271cad0cb739fa3fd22c
Charrier Clement <clement.charrier@etu.univ-orleans.fr>
2021-03-28 19:18:39 +0200

    Merge

---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/Pages/InscriptionPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaIllico.Mobile.Dtos;
+using PizzaIllico.Mobile.Dtos.Accounts;
+using PizzaIllico.Mobile.Dtos.Authentications;
+using PizzaIllico.Mobile.Services;
+using PizzaIllico.Mobile.ViewModels;
+using Storm.Mvvm.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PizzaIllico.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InscriptionPage : ContentPage
+    {
+        public InscriptionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonInscriptionClicked(object sender, EventArgs e)
+        {
+            UserViewModel.Register(email.Text, firstname.Text, lastname.Text, phone.Text, password.Text);
+
+        }
+
+    }
+}
